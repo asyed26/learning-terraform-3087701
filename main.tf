86e33717377539d3a6e02594f0c36b1691d943fb
asyed26 <129241135+asyed26@users.noreply.github.com>
2023-03-31 22:49:08 -0400

    Update main.tf
    Fixed typo

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name = ""blog_new"
+  name = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
 
